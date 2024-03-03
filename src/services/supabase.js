2a2c446932fb008e27d355ea6161dc89ce2856cf
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-03-03 22:56:08 +0100

    feat: upload an image om supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://cowarsufyzpgusddmzhq.supabase.co'
+export const supabaseUrl = 'https://cowarsufyzpgusddmzhq.supabase.co'
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvd2Fyc3VmeXpwZ3VzZGRtemhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk0NjQ3NzEsImV4cCI6MjAyNTA0MDc3MX0._KvI1URMTW8UMrJfSvYE_XyBE8w-xQR0LxEFKqg2B2Q"
 const supabase = createClient(supabaseUrl, supabaseKey)
 
